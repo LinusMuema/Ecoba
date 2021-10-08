a5b318f1e2f25122c71ec71cf0056446faf017a9
moose <linus.m.muema@gmail.com>
2021-10-08 16:24:43 +0300

    - added the redirect in splash screen

---
+++ app/src/main/java/com/moose/ecoba/presentation/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.moose.ecoba.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.moose.ecoba.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/moose/ecoba/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/moose/ecoba/presentation/splash/SplashActivity.kt
@@ -1,12 +1,37 @@
 package com.moose.ecoba.presentation.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.moose.ecoba.R
+import com.moose.ecoba.data.Preferences
+import com.moose.ecoba.presentation.home.HomeActivity
+import com.moose.ecoba.presentation.login.LoginActivity
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+import javax.inject.Inject
+import kotlin.concurrent.timerTask
 
+@AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var preferences: Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val navigate = timerTask {
+            // get the token
+            val token = preferences.getToken()
+            val context = this@SplashActivity
+
+            // navigate to required screen
+            if (token == null) startActivity(Intent(context, LoginActivity::class.java))
+            else startActivity(Intent(context, HomeActivity::class.java))
+            finish()
+        }
+        Timer().schedule(navigate, 3000)
     }
 }
